1f1133b5b4d3b681fbaf266b3bd8b3451fdb1706
Raymond <Raymond@DESKTOP-U671VEQ>
2021-05-15 10:46:38 -0400

    brand new reorganized repo with P0 and P1, removing old  ray-campbell repo

---
+++ Project1/src/com/revature/Driver.java
@@ -0,0 +1,40 @@
+package com.revature;
+
+import org.apache.log4j.BasicConfigurator;
+
+import com.revature.dao.EmployeeDaoImpl;
+import com.revature.dao.ManagerDaoImpl;
+import com.revature.models.Reimbursement;
+import com.revature.models.User;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		EmployeeDaoImpl e = new EmployeeDaoImpl();
+		ManagerDaoImpl m = new ManagerDaoImpl();
+		String mUsername = "manager";
+		String mPassword = "password";
+		User manager = new User(2, "employee", "password", "john", "smith", "johnsmith@reimb.com", 1);
+		String username = "employee";
+		String password = "password";
+		Reimbursement r = new Reimbursement(0, 0, password, 0, 0, 0, 0);
+		r.setReimbAmount(100.69);
+		r.setReimbDescription("practice");
+		r.setReimbAuthor(2);
+//		System.out.println(e.authenticateUser(username, password));
+//		System.out.println(e.submitReimbRequest(r));
+//		System.out.println(e.viewPendingReimb(2));
+//		e.testMethod();
+//		System.out.println(e.viewResolvedReimb(2));
+//		System.out.println(m.authenticateUser(mUsername, mPassword));
+//		System.out.println(m.viewAllReimbRequests());
+//		System.out.println(m.viewResolvedRequests());
+//		System.out.println(m.approveReimbRequest(3, 2));
+//		System.out.println(m.denyReimbRequest(3, 3));
+//		System.out.println(m.viewResolvedRequests());
+//		System.out.println(m.viewAllEmployees());
+		System.out.println(e.updateInfo(manager));
+	}
+
+}
---
+++ Project1/src/com/revature/dao/EmployeeDaoImpl.java
@@ -0,0 +1,251 @@
+package com.revature.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import com.revature.models.Reimbursement;
+import com.revature.models.User;
+import com.revature.utilities.ConnectionFactory;
+import com.revature.utilities.PasswordHashing;
+import com.revature.utilities.ProxyAppTwo;
+
+public class EmployeeDaoImpl implements EmployeeDao{
+	Connection c = ConnectionFactory.getConnectionFromEnv();
+	Scanner sc = new Scanner(System.in);
+	boolean ordering = true;
+	Logger loggy = Logger.getLogger(ProxyAppTwo.class);
+	@Override
+	public void login() {}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void submitReimbReceipt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public User authenticateUser(String username, String password) {
+		
+		User u = new User(0, username, password, null, null, null, 0);
+		String sql = "select * from project_one.users u where username = ? and password = ? and role_id = 1;";
+		
+		PreparedStatement ps;
+		try {
+			Connection conn = ConnectionFactory.getConnectionFromEnv();
+			ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, username);
+			ps.setString(2, PasswordHashing.doHashing(password));
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				u.setUserId(rs.getInt(1));
+				u.setPassword(rs.getString(3));
+				u.setFirstName(rs.getString(4));
+				u.setLastName(rs.getString(5));
+				u.setEmail(rs.getString(6));
+				u.setRoleId(rs.getInt(7));
+				
+				if(u.getUserId() > 0) {
+					loggy.info(u.getUsername() + " has logged in.");
+					return u;
+				}
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	public void testMethod() {
+		String sql = "select user_id from project_one.users u ;";
+		int number;
+		
+		
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				number = rs.getInt(1);
+				System.out.println(number);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+}
+
+	@Override
+	public boolean submitReimbRequest(Reimbursement reimbursement) {
+		String sql = "insert into project_one.reimbursements (reimb_amount, reimb_description,\r\n"
+				+ "reimb_author) values \r\n"
+				+ "(?, ?, ?);";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setDouble(1, reimbursement.getReimbAmount());
+			ps.setString(2, reimbursement.getReimbDescription());
+			ps.setInt(3, reimbursement.getReimbAuthor());
+			if(ps.executeUpdate() > 0) {
+				return true;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public List<Reimbursement> viewPendingReimb(int userId) {
+		List<Reimbursement> result = new ArrayList<Reimbursement>();
+		String sql = "select * from project_one.reimbursements r where reimb_author = ? and reimb_status_id = 1;";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, userId);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				result.add(new Reimbursement(
+						rs.getInt(1),
+						rs.getDouble(2),
+						rs.getString(5),
+						rs.getInt(7),
+						rs.getInt(8),
+						rs.getInt(9),
+						rs.getInt(10)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public List<Reimbursement> viewResolvedReimb(int userId) {
+		List<Reimbursement> result = new ArrayList<Reimbursement>();
+		String sql = "select * from project_one.reimbursements r where reimb_author = ? and reimb_status_id = 2 or reimb_status_id = 3;";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, userId);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				result.add(new Reimbursement(
+						rs.getInt(1),
+						rs.getDouble(2),
+						rs.getString(5),
+						rs.getInt(7),
+						rs.getInt(8),
+						rs.getInt(9),
+						rs.getInt(10)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public boolean updateInfo(User user) {
+		String sql = "update project_one.users set username = ?, password = ?, first_name = ?, last_name = ?, \r\n"
+				+ "email = ? where user_id = ?;";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setString(1, user.getUsername());
+			ps.setString(2, PasswordHashing.doHashing(user.getPassword()));
+			ps.setString(3, user.getFirstName());
+			ps.setString(4, user.getLastName());
+			ps.setString(5, user.getEmail());
+			ps.setInt(6, user.getUserId());
+			if(ps.executeUpdate() > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public User getUserById(int userId) {
+		
+		User u = new User(0, null, null, null, null, null, 0);
+		String sql = "select * from project_one.users u where user_id = ?;";
+		
+		PreparedStatement ps;
+		try {
+			Connection conn = ConnectionFactory.getConnectionFromEnv();
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, userId);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				u.setUserId(rs.getInt(1));
+				u.setPassword(rs.getString(3));
+				u.setFirstName(rs.getString(4));
+				u.setLastName(rs.getString(5));
+				u.setEmail(rs.getString(6));
+				u.setRoleId(rs.getInt(7));
+				
+				if(u.getUserId() > 0) {
+					loggy.info(u.getUsername() + " has logged in.");
+					return u;
+				}
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	@Override
+	public List<User> viewAllEmployees() {
+		List<User> results = new ArrayList<>();
+		String sql = "select * from project_one.users u where role_id = 1;";
+		try {
+			PreparedStatement ps = c.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				results.add(new User(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						rs.getString(5),
+						rs.getString(6),
+						rs.getInt(7)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return results;
+	}
+}
